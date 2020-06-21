7bf327bd0a5cfb86bfb8a1236c68754b433e19d8
VladisIove <vlad.shelemakha0302@gmail.com>
2020-06-21 16:43:34 +0300

    Refactoring and add obs pohibka

--- use_class/main.m
+++ use_class/main.m
@@ -10,10 +10,48 @@ clc; clear all; clear classes;
 % without noise -> DOSNT use method add_noise
 %% STEP 1
 % Create object with need starte parameters
+
+Info= {};
+answer = [];
+for name = ["","white", "pink", "grey", "broun", "blue", "violet"]
+    answer_signal = [];
+    Info(end+1)={name};
+    for s = [0.5, 1, 2]
+        for i = [1,2,3,4,5,6]
+            for j = [512,1024,2048,4096,8192]
+                for k = [2,20,200]
+                    signal = SignalClass();
+                    signal.p = i; % count periods
+                    signal.f = k; % frequency
+                    signal.N = j; % count dot
+                    signal.snr = s; % signal to noise ratio; optional value
+                    signal.na = 15% interpoletion
+                    %% STEP 2
+                    % Create first configuration 
+                    % Need use if you have reset data
+                    signal.updateValue();
+                    signal.add_noise(name);
+                    signal.MNKplusLagr();
+                    
+                    m = min(signal.PohOtn);
+                    text = ['snr ' num2str(signal.snr) ' p ' num2str(signal.p) ' N ' num2str(signal.N) ' f ' num2str(signal.f) ' min ' num2str(m)];
+                    Info(end+1)={text};
+                    answer_signal(end+1) = m;
+                end
+            end
+        end
+    end
+    answer = [answer; answer_signal];
+end
+    for i = 1:length(Info)
+        disp(Info(i));
+    end
+
+%{
 signal = SignalClass();
 signal.p = 1; % count periods
 signal.f = 2; % frequency
-signal.N = 256; % count dot
+signal.N = 512; % count dot
 signal.snr = 1/2; % signal to noise ratio; optional value
 signal.na = 15% interpoletion
 %% STEP 2
@@ -28,4 +66,6 @@ signal.updateValue();
 signal.MNKplusLagr();
 %% STEP 5
 % Plot
-signal.plot_signal();
+ signal.plot_signal();
+
+%}
